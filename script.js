8316dd2b7a3a28d417f7ca439dc61e9c6295b38c
GauravSumbe <gauravsumbe11@gmail.com>
2025-02-20 15:50:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let current = 'X';
 let arr = Array(9).fill(null);
 
 function checkWinner(){
-
-    if(
+  
+  if(
        ( arr[0] !== null && arr[0] == arr[1] && arr[1] == arr[2]) ||
         (arr[3] !== null && arr[3] == arr[4] && arr[4] == arr[5]) ||
         (arr[6] !== null && arr[6] == arr[7] && arr[7] == arr[8]) ||
@@ -14,18 +14,32 @@ function checkWinner(){
        ( arr[2] !== null && arr[2] == arr[4] && arr[4] == arr[6]) 
        ){
         console.log(current)  
-         document.write(`Winner is: ${current}`);
+        //  document.write(`Winner is: ${current}`);
+        const title=document.querySelector(".title");
+        title.innerHTML=(`Winner is: ${current}`);
          return;
        }
 
        if(!arr.some(e => e===null)){
-        document.write( `Draw !!`);
+        // document.write( `Draw !!`);
+        document.querySelector(".title").innerHTML="DRAW!!";
         return;
        }
      
 
 }
 
+const res_btn=document.querySelector(".res");
+res_btn.addEventListener('click',()=>{
+ 
+  console.log("here");
+  location.reload();
+//   arr = Array(9).fill(null);
+//   current = 'X';
+//   document.querySelector(".title").innerHTML="Tic Tac Toe";
+//   document.querySelector(".col").innerHTML="";
+}
+)
 function handleclick(el){
  const id = Number(el.id);
  if(arr[id]!== null) return;
